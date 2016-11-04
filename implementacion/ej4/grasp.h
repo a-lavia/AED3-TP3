fb7490f2579ea788824c00ae72c379573f231332
a-lavia <lavia.alejandro@gmail.com>
2016-11-04 05:19:21 -0300

    GRASP casi terminado, falta moverlo a su clase y hay que arreglar la busqueda local para que pueda usarse desde afuera

---
+++ implementacion/ej4/grasp.h
@@ -0,0 +1,22 @@
+#ifndef GRASP_H
+#define GRASP_H
+
+#include <vector>
+
+class grasp {
+	public:
+		grasp();
+	private:
+
+	vector<pos> gimnasios(n);
+	vector<int> gimnasiosPoder(n);
+	vector<pos> paradas(m);
+	
+
+};
+
+grasp::grasp() {
+
+}
+
+#endif
