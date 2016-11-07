b9591ff8f1587b01702670fa340cac29aa34f158
a-lavia <lavia.alejandro@gmail.com>
2016-11-06 22:02:30 -0300

    Implementacion de GRASP

--- implementacion/ej4/ej.cpp
+++ implementacion/ej4/ej.cpp
@@ -1,126 +1,11 @@
-
-//float distanciaCamino(queue<int> camino, vector<pos>& posGym, vector<pos>& posParada);
-
-
-list<pair<int, float>> obtenerCandidatos() {
-	list<pair<int, float>> candidatos;
-
-	//Agrego como candidatos iniciales todas las paradas y gimnasios que pueden derrotarse inicialmente
-	for(int i = 0; i < nodos; i++) {
-		if(!(esGimnasio(i) && gimnasiosPoder[i] == 0)) continue;
-		candidatos.push_back(pair<int, float>(i, 0);
-	}
-
-	return candidatos;
-}
-
-vector<int> obtenerCandidatosRestringidos(list<pair<int, float>>& candidatos, float alfa) {
-	vector<int> candidatosRestringidos;
-
-	//Obtengo la maxima y minima distancia
-	float cMax = candidatos.front().second;
-	float cMin = candidatos.front().second;
-	for(pair<int, float>& candidato : candidatos) {
-		if(candidato.second > cMax) cMax = candidato.second;
-		if(candidato.second < cMin) cMin = candidato.second;
-	}
-
-	//Agrego los candidatos que cumplen con el threshold
-	float threshold = cMin + alfa * (cMax - cMin);
-	for(pair<int, float>& candidato : candidatos) {
-		if(candidato.second < threshold) candidatosRestringidos.insert(candidato.first);
-	}
-
-	return candidatosRestringidos;
-}
-
-void actualizarCandidatos(list<pair<int, float>>& candidatos, queue<int>& solucionParcial, int pociones) {
-
-	//Actualizo todos los candidatos
-	for(list<pair<int, float>>::iterator it = candidatos.begin(); it != candidatos.end(); it++) {
-
-		pair<int, float>& candidato = *it;
-
-		//Remuevo gimnasios a los que no puedo ganarle
-		if(esGimnasio(candidato.first) && gimnasioPoder[candidato.first] > pociones) {
-			it = candidatos.erase(it);
-			it--;
-			continue;
-		}
-
-		//Actualizo las distancias con respecto a la última posición
-		candidato.second = distancia(candidato.first, solucionParcial.back());
-		
-	}
-
-	//Me fijo que gimnasios visite en la solucion parcial
-	bool gymVisitado[gimnasios.size()];
-	for(int i = 0; i < gimnasios.size(); i++) gymVisitado[i] = false;
-
-	queue<int> sol = solucionParcial;
-	while(!sol.empty()) {
-		int n = sol.front(); sol.pop();
-		if(esGimnasio(n)) gymVisitado[n] = true;
-	}
-
-	//Agrego todos los nuevos gimnasios que puedo visitar
-	for(int i = 0; i < gimnasios.size(); i++) {
-		if(!gymVisitado[i] && gimnasioPoder[i] <= pociones) 
-			candidato.push_back(pair<int, float>(i, distancia(i, solucionParcial.back()));
-	}
-
-}
-
-queue<int> graspSolucionAleatoria(float alfa) {
-
-	queue<int> solucionParcial;
-
-	//Obtengo todos los candidatos iniciales
-	list<pair<int, float>> candidatos = obtenerCandidatos();
-	
-	int pociones = 0;
-	int gimnasiosRestantes = gimnasios.size();
-	while(gimnasiosRestantes > 0) {
-
-		//Restrinjo los candidatos a los mejores segun un threshold alfa (parte greedy)
-		vector<int> candidatosRestringidos = obtenerCandidatosRestringidos(candidatos, alfa);
-
-		//Obtengo un candidato aleatorio (parte probabilistica)
-		int candidatoAleatorio = candidatosRestringidos[random(0, candidatosRestringidos.size())];
-
-		if(esGimnasio(candidatoAleatorio)) {
-			gimnasiosRestantes--;
-			pociones -= 3;
-		}
-		else pociones = min(pociones+3, mochila);
-
-		//Lo agrego a la solución a construir (parte adaptativa)
-		solucionParcial.push(candidatoAleatorio);
-
-		//Actualizo la lista de candidatos
-		actualizarCandidatos(candidatos, solucionParcial, pociones);
-	}
-
-	return solucionParcial;
-}
-
-void grasp(int maxIteraciones, int semilla) {
-	srand(semilla);
-	queue<int> solucion;
-	float dist;
-	for(int i = 0; i < maxIteraciones; i++) {
-		queue<int> solucion = graspSolucionAleatoria(0.5, 0);
-
-		Camino bl = Camino(g, mochila);
-        //void asignarDistancia(float distancia);
-        //void asignarCamino(queue<int>& caminoCola);
-		//bl.busquedaLocal(permutaCamino);
-		//bl.obtenerSolucion()...
-		//si es mejor solución que la actual reemplazar
-	}
-}
+#include "grasp.h"
 
 int main(int argc, char* argv[]) {
+
+	int iteraciones;
+	cin >> iteraciones;
+	int semilla;
+	cin >> semilla;
 
 	int n;
 	cin >> n;
@@ -138,31 +23,14 @@ int main(int argc, char* argv[]) {
 		cin >> gimnasios[i].second;
 		cin >> gimnasiosPoder[i];
 	}
+
 	for(int i = 0; i < m; i++) {
 		cin >> paradas[i].first;
 		cin >> paradas[i].second;
 	}
 
-	//Armo el grafo para la búsqueda local
-	Grafo g(n+m);
+	grasp g = grasp(gimnasios, gimnasiosPoder, paradas, mochila);
+	cout << g.correr_grasp(iteraciones, semilla) << endl;
 
-	for(int nodo = 0; i < n+m; i++) {
-		bool esGym = i < n;
-		Nodo nodoNuevo;
-		nodoNuevo.id = nodo;
-		nodoNuevo.x = esGym ? gimnasios[i].first : paradas[i].first;
-		nodoNuevo.y = esGym ? gimnasios[i].second : paradas[i].second;
-		nodoNuevo.pociones = esGym ? gimnasiosPoder[i] : 3;
-		nodoNuevo.gimnasio = esGym;
-		g.asignarNodo(nodoNuevo);
-	}
-    for(int i = 0; i < n+m; i++){
-        for(int j = 0; j < n+m; j++){
-            Nodo& n1 = g.nodos()[i];
-            Nodo& n2 = g.nodos()[j];
-            g.asignarDistancia(n1, n2, distanciaNodos(n1, n2));
-        }
-    }
-
+	return 0;
 }
-
--- implementacion/ej4/grasp.h
+++ implementacion/ej4/grasp.h
@@ -1,22 +1,54 @@
 #ifndef GRASP_H
 #define GRASP_H
 
+#include <utility> //pair
 #include <vector>
+#include <queue>
+#include <list>
+
+#include "../ej3/Camino.h"
+#include "../ej3/Grafo.h"
+
+using namespace std;
 
 class grasp {
+
 	public:
-		grasp();
+
+		grasp(vector<pos>& g, vector<int>& gp, vector<pos>& p, int m);
+
+		float correr_grasp(int maxIteraciones, int semilla);
+
+		//float correr_grasp_reactivo(int maxIteraciones);
+
 	private:
 
-	vector<pos> gimnasios(n);
-	vector<int> gimnasiosPoder(n);
-	vector<pos> paradas(m);
-	
+		//
+		int aleatorio(int min, int max);
+
+		bool esGym(int i);
+
+		float distancia(int a, int b);
+
+		float distanciaCamino(queue<int> camino);
+
+		//
+		list<pair<int, float>> obtenerCandidatos();
+
+		void actualizarCandidatos(list<pair<int, float>>& candidatos, vector<bool>& visitados, int actual, int pociones);
+
+		vector<int> obtenerCandidatosRestringidos(list<pair<int, float>>& candidatos, float alfa);
+
+		queue<int> graspSolucionAleatoria(float alfa);
+
+		//
+		vector<pos> gimnasios;
+		vector<int> gimnasiosPoder;
+		vector<pos> paradas;
+		int mochila;
+
+		Grafo grafo;
 
 };
-
-grasp::grasp() {
-
-}
 
 #endif
