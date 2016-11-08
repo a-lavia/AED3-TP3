35fea75192666781fbbe757293da054fb59abe1e
Horacio Gomez <horaciogomez.1993@gmail.com>
2016-11-08 11:44:20 -0300

    script fix

---
+++ experimentacion/ej1/limpiar.sh
@@ -0,0 +1,1 @@
+rm -f rj1.out nohup.out mapa.out
--- experimentacion/ej1/unt.sh
+++ experimentacion/ej1/unt.sh
@@ -1,13 +1,20 @@
 rm -f ej1.out
 touch ej1.out
 
+
+ceiling_divide() {
+  ceiling_result=$((($1+$2-1)/$2))
+}
+
+
 for i in {3..14..1} 
 do
-    paradas=$(($i * 2 / 3))
-    gimnasios=$(($i / 3))
-    mochila=$(($paradas * 3 / $gimnasios + 1))
+    paradas=$((($i*2+3-1)/3))
+    gimnasios=$(($i/3))
+    mochila=$(($paradas*3/$gimnasios+1))
     for dificultad in {10..100..10}
     do
+        # echo $i"   "$paradas", "$gimnasios", "$mochila
         ./gen.out 20 20 $gimnasios $paradas $mochila $dificultad 10
         ./ej1Exp.out < mapa.out
     done
